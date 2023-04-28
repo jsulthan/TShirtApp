3e4312a9a3b6bdc432045dd2d896646bd385dc4d
jsulthan <sulthanjamaal@gmail.com>
2023-04-28 12:23:22 -0400

    5th commit

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -52,7 +52,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('https://tshirtapp.onrender.com/', {
+      const response = await fetch('https://tshirtapp.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
